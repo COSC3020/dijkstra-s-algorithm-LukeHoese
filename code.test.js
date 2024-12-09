f976c8c31141cbfc16e6b789fc9b49e5c74d12bc
LukeHoese <157862417+LukeHoese@users.noreply.github.com>
2024-12-09 12:32:10 -0700

    Update code.test.js

--- code.test.js
+++ code.test.js
@@ -16,3 +16,18 @@ assert(JSON.stringify(d1) == JSON.stringify({A: 0, B: 1, C: 3, D: 4}));
 const graph2 = {};
 const d2 = dijkstra(graph2, 'A');
 assert(JSON.stringify(d2) == JSON.stringify({}));
+
+const graph3 = {
+    A: {}
+};
+const d3 = dijkstra(graph3, 'A');
+assert(JSON.stringify(d3) == JSON.stringify({A: null}));
+    
+const graph4 = {
+    A: { B: 1 },
+    B: { A: 1 },
+    C: { D: 2 },
+    D: { C: 2 }
+};
+const d4 = dijkstra(graph4, 'A');
+assert(JSON.stringify(d4) == JSON.stringify({A: 0, B: 1, C: Infinity, D: Infinity}));
