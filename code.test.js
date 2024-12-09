ed76ff17a53c3ed3af4fbc58155ce206f835b472
LukeHoese <157862417+LukeHoese@users.noreply.github.com>
2024-12-09 12:17:29 -0700

    Update code.test.js

--- code.test.js
+++ code.test.js
@@ -14,5 +14,5 @@ const { distances } = dijkstra(graph, 'A');
 assert(JSON.stringify(distances) == JSON.stringify({A: 0, B: 1, C: 3, D: 4}));
 
 const graph2 = {};
-const { distances2 } = dijkstra(graph, 'A');
+const { distances2 } = dijkstra(graph2, 'A');
 assert(JSON.stringify(distances2) == JSON.stringify({}));
