52b4ed860cbade5b3c293b76db21fc2cec4f45b4
LukeHoese <157862417+LukeHoese@users.noreply.github.com>
2024-12-09 12:15:41 -0700

    Update code.test.js

--- code.test.js
+++ code.test.js
@@ -10,6 +10,9 @@ const graph = {
     C: { A: 4, B: 2, D: 1 },
     D: { B: 5, C: 1 }
 };
-const { distances, previous } = dijkstra(graph, 'A');
+const { distances } = dijkstra(graph, 'A');
 assert(JSON.stringify(distances) == JSON.stringify({A: 0, B: 1, C: 3, D: 4}));
-assert(JSON.stringify(shortestPath(previous, 'D') == JSON.stringify((4) ['A', 'B', 'C', 'D'])));
+
+const graph2 = {};
+const { distances } = dijkstra(graph, 'A');
+assert(JSON.stringify(distances) == JSON.stringify({}));
